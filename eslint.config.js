2f08cf2dc77b54a7dd711d742ecd3a3464404b8b
dandehoon <nguyenbao1917@gmail.com>
2025-06-22 20:37:02 +0700

    chore: update Node.js version and ESLint config for consistency [patch]

--- eslint.config.js
+++ eslint.config.js
@@ -20,7 +20,7 @@ export default [
     languageOptions: {
       parser: tsParser,
       parserOptions: {
-        ecmaVersion: 2020,
+        ecmaVersion: 2022,
         sourceType: 'module',
       },
     },
@@ -34,7 +34,7 @@ export default [
     languageOptions: {
       parser: tsParser,
       parserOptions: {
-        ecmaVersion: 2020,
+        ecmaVersion: 2022,
         sourceType: 'module',
       },
       globals: {
